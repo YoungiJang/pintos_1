a6cb4cd156ffbb3758f1108006181c1873247496
YoungiJang <131528863+YoungiJang@users.noreply.github.com>
2023-10-09 17:42:56 +0900

    Create calculate.h

---
+++ pintos/src/threads/calculate.h
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#define F 1<<14
+
+int int_fp(int n)
+{
+    return n * F;
+}
+
+int fp_int_zero(int x)
+{
+    return x / F;
+}
+
+int fp_int_round(int x) {
+    if (x >= 0)
+    {
+        return (x + F / 2) / F;
+    }
+    else
+    {
+        return (x - F / 2) / F;
+    }
+}
+
+
+int add_fp_fp(int x, int  y) {
+    return x + y;
+}
+
+int subtract_fp_fp(int x, int y)
+{
+
+    return x - y;
+}
+
+int add_int_fp(int x, int n)
+{
+    return x + n * F;
+}
+
+int subtract_int_fp(int x, int n)
+{
+    return x - n * F;
+}
+
+int multiply_fp(int x, int y)
+{
+    return ((int64_t)x) * y / F;
+}
+
+int multiply_int(int x, int n)
+{
+    return x * n;
+}
+
+int divide_fp(int x, int y)
+{
+    return ((int64_t)x) * y / F;
+}
+
+int divide_int(int x, int n)
+{
+    return x / n;
+}
