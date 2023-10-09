e54b1c97e241535576d73f0b78dc4173f2c5fb5f
Dfkenin <101795537+Dfkenin@users.noreply.github.com>
2023-10-09 21:42:54 +0900

    Update calculate.h

--- pintos/src/threads/calculate.h
+++ pintos/src/threads/calculate.h
@@ -1,5 +1,16 @@
 #include <stdint.h>
 #define F 1<<14
+int int_fp(int n);
+int fp_int_zero(int x);
+int fp_int_round(int x);
+int add_fp_fp(int x, int  y);
+int subtract_fp_fp(int x, int y);
+int add_int_fp(int x, int n);
+int subtract_int_fp(int x, int n);
+int multiply_fp(int x, int y);
+int multiply_int(int x, int n);
+int divide_fp(int x, int y);
+int divide_int(int x, int n);
 
 int int_fp(int n)
 {
